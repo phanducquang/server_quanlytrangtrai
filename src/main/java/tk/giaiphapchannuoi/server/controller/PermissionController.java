bdde88e1c18a97ce0f3fa6df3424815370575bf6
Đức Quang <phanducquang07@gmail.com>
2019-06-14 00:24:32 +0700

    cập nhật link api permission

--- src/main/java/tk/giaiphapchannuoi/server/controller/PermissionController.java
+++ src/main/java/tk/giaiphapchannuoi/server/controller/PermissionController.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping(value = "/api/permisssion")
+@RequestMapping(value = "/api/permission")
 public class PermissionController {
 
     @Autowired
